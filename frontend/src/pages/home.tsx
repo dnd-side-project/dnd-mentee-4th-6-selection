c70359d286cafc596b26989ede8dab50c8ac37ee
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2021-01-25 22:53:02 +0900

    feat: Add Sidebar

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { Sidebar } from "../components/sidebar";
 
 export const Home = () => {
+  const [toggleSidebar, setToggleSigebar] = useState(false);
+  const sidebarOnClick = () => {
+    setToggleSigebar(!toggleSidebar);
+  };
+
   return (
     <div>
       <Helmet>
         <title>Selection</title>
       </Helmet>
+      <button onClick={sidebarOnClick}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      {toggleSidebar && (
+        <>
+          <Sidebar onClick={sidebarOnClick} />
+        </>
+      )}
       <h2>Home</h2>
     </div>
   );
