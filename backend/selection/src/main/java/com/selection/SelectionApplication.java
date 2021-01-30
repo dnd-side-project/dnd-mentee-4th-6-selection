3b06a843a318a788de9ef2a91fdf1fdde3461c2e
김예림 <kiel0103@naver.com>
2021-01-30 12:45:52 +0900

    feat: oauth configuration 작성

--- backend/selection/src/main/java/com/selection/SelectionApplication.java
+++ backend/selection/src/main/java/com/selection/SelectionApplication.java
@@ -1,8 +1,11 @@
 package com.selection;
 
+import com.selection.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class SelectionApplication {
 
