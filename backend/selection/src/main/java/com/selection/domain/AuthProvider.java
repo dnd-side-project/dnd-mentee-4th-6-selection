19badc22083e19f0d90726629441aacc2c10b2bd
김예림 <kiel0103@naver.com>
2021-01-30 13:04:44 +0900

    feat: User 클래스 작성

---
+++ backend/selection/src/main/java/com/selection/domain/AuthProvider.java
@@ -0,0 +1,7 @@
+package com.selection.domain;
+
+public enum  AuthProvider {
+    GOOGLE,
+    KAKAO,
+    NAVER;
+}
---
+++ backend/selection/src/main/java/com/selection/domain/user/User.java
@@ -0,0 +1,48 @@
+package com.selection.domain.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.security.AuthProvider;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "email")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean emailVerified = false;
+
+    @JsonIgnore
+    private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+
+    private String providerId;
+
+    // Getters and Setters (Omitted for brevity)
+}
