f0ca153bfae53469f397c796b69343f1d6054a6e
김예림 <kiel0103@naver.com>
2021-01-30 13:06:04 +0900

    feat: UserRepository 작성

---
+++ backend/selection/src/main/java/com/selection/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.selection.repository;
+
+import com.selection.domain.user.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
