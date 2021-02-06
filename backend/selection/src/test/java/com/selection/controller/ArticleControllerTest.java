a698493032a9937fe81e8508932b74641eff8e32
WonJongWoon <delphi3228@pukyong.ac.kr>
2021-02-07 00:10:57 +0900

    feat: 최신글 API 테스트 코드 추가

--- backend/selection/src/test/java/com/selection/controller/ArticleControllerTest.java
+++ backend/selection/src/test/java/com/selection/controller/ArticleControllerTest.java
@@ -10,6 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.selection.domain.article.Article;
+import com.selection.dto.article.ArticleLatestResponse;
 import com.selection.dto.article.ArticleModifyRequest;
 import com.selection.dto.article.ArticleSaveRequest;
 import com.selection.dto.question.QuestionModifyRequest;
@@ -17,6 +18,7 @@ import com.selection.dto.question.QuestionSaveRequest;
 import com.selection.dto.tag.TagModifyRequest;
 import com.selection.dto.tag.TagSaveRequest;
 import com.selection.repository.ArticleRepository;
+import com.selection.service.article.ArticleService;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,6 +32,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -51,6 +55,9 @@ class ArticleControllerTest {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @Autowired
+    private ArticleService articleService;
+
     @BeforeEach
     @DisplayName("테스트 목업 준비")
     public void setUp() {
@@ -60,11 +67,7 @@ class ArticleControllerTest {
             .build();
     }
 
-    @Test
-    @Order(1)
-    @DisplayName("게시글 작성 API 테스트")
-    public void createArticle() throws Exception {
-        // given
+    public ArticleSaveRequest createArticleSaveRequest() {
         final String title = "제목 1";
         final String content = "내용 1";
         final String backgroundColor = "#FFFFFF";
@@ -81,13 +84,21 @@ class ArticleControllerTest {
         tags.add(new TagSaveRequest(tag1));
         tags.add(new TagSaveRequest(tag2));
 
-        ArticleSaveRequest createArticle = ArticleSaveRequest.builder()
+        return ArticleSaveRequest.builder()
             .title(title)
             .content(content)
             .backgroundColor(backgroundColor)
             .questions(questions)
             .tags(tags)
             .build();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("게시글 작성 API 테스트")
+    public void createArticle() throws Exception {
+        // given
+        ArticleSaveRequest createArticle = createArticleSaveRequest();
 
         // when
         mockMvc.perform(post("/articles")
@@ -98,9 +109,9 @@ class ArticleControllerTest {
 
         // then
         Article article = articleRepository.findAll().get(0);
-        assertThat(article.getTitle()).isEqualTo(title);
-        assertThat(article.getContent()).isEqualTo(content);
-        assertThat(article.getBackgroundColor()).isEqualTo(backgroundColor);
+        assertThat(article.getTitle()).isEqualTo(createArticle.getTitle());
+        assertThat(article.getContent()).isEqualTo(createArticle.getContent());
+        assertThat(article.getBackgroundColor()).isEqualTo(createArticle.getBackgroundColor());
     }
 
     @Test
@@ -185,5 +196,29 @@ class ArticleControllerTest {
         // then
         List<Article> articles = articleRepository.findAll();
         assertThat(articles.size()).isEqualTo(0);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("최신글 API 테스트")
+    public void lookUpLatestArticle() throws Exception {
+        // given
+        ArticleSaveRequest article1 = createArticleSaveRequest();
+        ArticleSaveRequest article2 = createArticleSaveRequest();
+        articleRepository.save(article1.toEntity());
+        articleRepository.save(article2.toEntity());
+
+        List<ArticleLatestResponse> articleLatestResponses = articleService.lookUpLatest(10L);
+
+        // when
+        MvcResult result = mockMvc.perform(
+            get("/articles/latest")
+        )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+
+        // then
+        assertThat(result.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(articleLatestResponses));
     }
 }
