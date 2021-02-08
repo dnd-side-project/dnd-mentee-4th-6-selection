9aed94b39b590d495ddea95f7463f0d0eed53070
김예림 <kiel0103@naver.com>
2021-02-08 14:34:16 +0900

    feat: naver 로그인 추가

---
+++ backend/selection/src/main/java/com/selection/security/oauth/user/NaverOAuth2UserInfo.java
@@ -0,0 +1,30 @@
+package com.selection.security.oauth.user;
+
+import java.util.Map;
+
+public class NaverOAuth2UserInfo extends OAuth2UserInfo {
+
+    public NaverOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return ((String) attributes.get("id"));
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getImageUrl() {
+        return (String) attributes.get("avatar_url");
+    }
+}
