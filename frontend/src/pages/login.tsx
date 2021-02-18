9757c40cdb8164b6ee2bcdaa65c1e5da82b9e6c8
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2021-02-19 00:33:29 +0900

    feat: Login UI, Naver Login

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,38 +3,9 @@ import { Redirect } from "react-router-dom";
 import styled from "styled-components";
 import { Helmet } from "react-helmet-async";
 import { BACKEND_URL, FRONTEND_URL } from "../constants";
-
-const LoginContainer = styled.div`
-  margin-top: 200px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const SocialLink = styled.a`
-  text-decoration: none;
-  color: black;
-  width: 380px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
-  font-weight: 600;
-  background-color: #dddddd;
-  margin-bottom: 13px;
-  transition: background-color 0.1s ease-in-out;
-  &:visited {
-    color: black;
-  }
-  &:hover {
-    background-color: #bbbbbb;
-  }
-  &:active {
-    background-color: #909090;
-  }
-`;
+import icon_purpleguma from "../styles/img/icon_purpleguma_max.svg";
+import icon_google from "../styles/img/icon_google_logo.jpeg";
+import icon_naver from "../styles/img/icon_naver_logo.png";
 
 interface IProps {
   authenticated: boolean;
@@ -50,21 +21,28 @@ export const Login: React.FC<IProps> = ({ authenticated }: IProps) => {
             <title>로그인 - GO!GUMA</title>
           </Helmet>
           <div>
+            <LoginTitleContainer>
+              <LoginImg src={icon_purpleguma} />
+              <LoginTitle>고구마</LoginTitle>
+              <LoginSubtitle>
+                고민, 그만!
+                <br />
+                고구마에서 다~ 털어놓고가세요
+              </LoginSubtitle>
+            </LoginTitleContainer>
             <LoginContainer>
               <SocialLink
+                className={"google"}
                 href={`${BACKEND_URL}/oauth2/authorize/google?redirect_uri=${FRONTEND_URL}/oauth2/redirect`}
               >
-                <span>구글 계정으로 로그인</span>
+                <img src={icon_google} width={34} height={34} />
+                <div>구글 계정으로 로그인</div>
               </SocialLink>
               <SocialLink
-                href={`http://localhost:8080/oauth2/authorize/naver?redirect_uri=http://localhost:3000/oauth2/redirect`}
+                href={`${BACKEND_URL}/oauth2/authorize/naver?redirect_uri=${FRONTEND_URL}/oauth2/redirect`}
               >
-                <span>네이버 계정으로 로그인</span>
-              </SocialLink>
-              <SocialLink
-                href={`http://localhost:8080/oauth2/authorize/kakaotalk?redirect_uri=http://localhost:3000/oauth2/redirect`}
-              >
-                <span>카카오 계정으로 로그인</span>
+                <img src={icon_naver} width={34} height={34} />
+                <div>네이버 계정으로 로그인</div>
               </SocialLink>
             </LoginContainer>
           </div>
@@ -73,3 +51,72 @@ export const Login: React.FC<IProps> = ({ authenticated }: IProps) => {
     </>
   );
 };
+
+const LoginTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 111px;
+  margin-bottom: 249px;
+`;
+
+const LoginImg = styled.img`
+  width: 78px;
+  height: 78px;
+`;
+
+const LoginTitle = styled.div`
+  font-family: "Gaegu", cursive;
+  font-size: 53px;
+  color: #8c5cdd;
+  margin-bottom: 23px;
+`;
+
+const LoginSubtitle = styled.div`
+  font-family: "Spoqa Han Sans Neo", "sans-serif";
+  font-size: 14px;
+  color: #989898;
+  text-align: center;
+  line-height: 21px;
+`;
+
+const LoginContainer = styled.div`
+  width: 100%;
+  padding: 0 17px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SocialLink = styled.a`
+  text-decoration: none;
+  width: 100%;
+  height: 45px;
+  border-radius: 7px;
+  font-family: "Spoqa Han Sans Neo", "sans-serif";
+  font-size: 14px;
+  font-weight: 500;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+
+  &:first-child {
+    color: black;
+    border: 1px solid #e8e8e8;
+    & > div {
+      padding-left: 33px;
+      padding-right: 67px;
+    }
+  }
+  &:nth-child(2) {
+    color: white;
+    background-color: #1ec800;
+    & > div {
+      padding-left: 25px;
+      padding-right: 59px;
+    }
+  }
+`;
