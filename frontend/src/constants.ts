e2b762f01df11a24b88bc1ac38d9691988f7e05f
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2021-02-26 23:43:49 +0900

    fix: fronturl fix

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
 export const BACKEND_URL = "http://ec2-15-165-52-165.ap-northeast-2.compute.amazonaws.com:8080";
-export const FRONTEND_URL = "https://goguma.netlify.app/";
+export const FRONTEND_URL = "https://goguma.netlify.app";
